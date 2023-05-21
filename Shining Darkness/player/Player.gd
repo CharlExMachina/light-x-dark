b6769236fa5d4144d9347f18718fae86cddf23b0
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2023-05-21 16:58:12 -0600

    Adds light and dark beams
    Adds dark beam
    
    Adds light beam
    
    
    Adds placeholder sound

--- Shining Darkness/player/Player.gd
+++ Shining Darkness/player/Player.gd
@@ -11,12 +11,18 @@ enum AttackMode {
 }
 
 onready var _ship_sprite = $PlayerShipSprite
+onready var _dark_beam_timer = $DarkBeamDelay
+onready var _light_beam_timer = $LightBeamDelay
+
+var _light_beam_scene = preload("res://player/beams/LightBeam.tscn")
+var _dark_beam_scene = preload("res://player/beams/DarkBeam.tscn")
 
 var _velocity: Vector2
 var _movement: Vector2
 var _x_limits: float
 var _sprite_size: float
-var _can_shoot: bool = true
+var _can_shoot_light_beam: bool = true
+var _can_shoot_dark_beam: bool = true
 var _attack_mode = AttackMode.LIGHT_MODE
 
 var x_acc = 0.0
@@ -36,10 +42,15 @@ func _physics_process(delta: float) -> void:
 	_handle_movement()
 	_handle_mode_switch()
 
+	if Input.is_action_pressed("shoot"):
+		_shoot_beam()
+
+
 func _handle_movement() -> void:
 	_velocity = _movement.normalized()
 	_velocity = move_and_slide(_velocity * movement_speed)
 	position.x = clamp(position.x, 0 + _sprite_size, _x_limits - _sprite_size)
+
 
 func _handle_mode_switch() -> void:
 	if (Input.is_action_just_pressed("light_mode_switch") and not _attack_mode == AttackMode.LIGHT_MODE):
@@ -48,3 +59,26 @@ func _handle_mode_switch() -> void:
 	elif (Input.is_action_just_pressed("dark_mode_switch") and not _attack_mode == AttackMode.DARK_MODE):
 		_attack_mode = AttackMode.DARK_MODE
 		_ship_sprite.switch_to_dark_mode()
+
+
+func _shoot_beam() -> void:
+	if _can_shoot_light_beam and _attack_mode == AttackMode.LIGHT_MODE:
+		_can_shoot_light_beam = false
+		var beam_instance = _light_beam_scene.instance()
+		get_parent().add_child(beam_instance)
+		beam_instance.position = position - Vector2(0, 20)
+		_light_beam_timer.start()
+	elif _can_shoot_dark_beam and _attack_mode == AttackMode.DARK_MODE:
+		_can_shoot_dark_beam = false
+		var beam_instance = _dark_beam_scene.instance()
+		get_parent().add_child(beam_instance)
+		beam_instance.position = position - Vector2(0, 20)
+		_dark_beam_timer.start()
+
+
+func _on_LightBeamDelay_timeout() -> void:
+	_can_shoot_light_beam = true
+
+
+func _on_DarkBeamDelay_timeout() -> void:
+	_can_shoot_dark_beam = true
