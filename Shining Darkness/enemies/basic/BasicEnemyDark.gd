7b05ef850eba2d62aceee0ef64fd112f39810d3a
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2023-05-21 20:45:28 -0600

    Light and dark enemies can be destroyed

---
+++ Shining Darkness/enemies/basic/BasicEnemyDark.gd
@@ -0,0 +1,24 @@
+extends "res://enemies/BaseEnemy.gd"
+
+
+func _on_Area2D_area_entered(area: Area2D) -> void:
+	var damage = area.damage_value
+
+	print(health_points)
+
+	health_points -= damage
+
+	if (health_points <= 0):
+		$AnimatedSprite.hide()
+		$Area2D.set_deferred("monitoring", false)
+		$Area2D.set_deferred("monitorable", false)
+		$TimeToShoot.stop()
+		$ExplosionAnimation.play("default")
+
+
+func _on_TimeToShoot_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_ExplosionAnimation_animation_finished() -> void:
+	queue_free()
